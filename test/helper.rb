075dc9cb57d6460ab0b0b0e699b408d8fbe0f38b
Chaitanya Phalak <cphalak@splunk.com>
2018-10-01 13:21:16 -0700

    swap out kubelet api, bug-fixes, multiple nodenames support, added unit tests and code coverage

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,39 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
 require "test-unit"
 require "fluent/test"
 require "fluent/test/driver/input"
 require "fluent/test/helpers"
+require "webmock/test_unit"
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
+
+module PluginTestHelper
+
+  def k8s_host() "127.0.0.1" end
+  def k8s_port() "8001" end
+  def k8s_url(path='api') "https://#{k8s_host}:#{k8s_port}/#{path}" end
+
+  def kubelet_summary_api_url() "http://generics-aws-node-name:10255/stats/summary" end
+  def stub_k8s_requests
+    # all stub response bodies are from real k8s 1.8 API calls
+    stub_kubelet_summary_api
+  end
+
+  def stub_kubelet_summary_api
+    open(File.expand_path('../unit.json', __FILE__)).tap { |f|
+      stub_request(:get, "#{kubelet_summary_api_url}").to_return(body: f.read())
+    }.close
+  end
+
+  def get_parsed_string
+    parsed_string = nil
+    open(File.expand_path('../unit.json', __FILE__)).tap { |f|
+      parsed_string = f.read()
+    }.close
+    parsed_string
+  end
+end
--- test/plugin/test_in_kubernetes_metrics.rb
+++ test/plugin/test_in_kubernetes_metrics.rb
@@ -2,17 +2,150 @@ require "helper"
 require "fluent/plugin/in_kubernetes_metrics.rb"
 
 class KubernetesMetricsInputTest < Test::Unit::TestCase
+  include Fluent::Test::Helpers
+  include PluginTestHelper
+
+  @@driver = nil
+
+  @@hash_map_test = Hash.new
+
+  CONFIG = %[
+      type kubernetes_metrics
+      node_name generics-aws-node-name
+      tag kube.*
+      insecure_ssl true
+      interval 10s
+      use_rest_client true
+  ]
+  
   setup do
     Fluent::Test.setup
+
+    @@parsed_string = JSON.parse(get_parsed_string)
+
+    stub_k8s_requests
+
+    @@driver = create_driver
+    @@driver.run(timeout:20,  expect_emits: 1, shutdown: true)
+
+    @@driver.events.each do |tag, time, record|
+      @@hash_map_test[tag] = tag, time, record
+    end
+
+
   end
 
-  test "failure" do
-    flunk
-  end
-
-  private
-
-  def create_driver(conf)
+  def create_driver(conf = CONFIG)
     Fluent::Test::Driver::Input.new(Fluent::Plugin::KubernetesMetricsInput).configure(conf)
   end
+
+  test 'configuration' do
+    assert_nothing_raised(Fluent::ConfigError) {
+      create_driver(CONFIG)
+    }
+
+    d = create_driver
+    assert_equal 'generics-aws-node-name', d.instance.node_name
+    assert_equal 'kube.*', d.instance.tag
+    assert_equal true, d.instance.insecure_ssl
+    assert_equal 10, d.instance.interval
+    assert_equal true, d.instance.use_rest_client
+  end
+
+  sub_test_case "node_unit_tests" do
+
+    test 'test_emit_cpu_metrics' do
+
+      assert_not_nil @@hash_map_test.find('kube.node.cpu.usage')
+      assert_equal @@parsed_string['node']['cpu']['usageNanoCores'], @@hash_map_test['kube.node.cpu.usage'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.cpu.usage_rate')
+      assert_equal @@parsed_string['node']['cpu']['usageNanoCores']/ 1_000_000, @@hash_map_test['kube.node.cpu.usage_rate'][2]["value"]
+
+    end
+
+    test 'test_emit_memory_metrics' do
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.available_bytes')
+      assert_equal @@parsed_string['node']['memory']['availableBytes'], @@hash_map_test['kube.node.memory.available_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.usage_bytes')
+      assert_equal @@parsed_string['node']['memory']['usageBytes'], @@hash_map_test['kube.node.memory.usage_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.working_set_bytes')
+      assert_equal @@parsed_string['node']['memory']['workingSetBytes'], @@hash_map_test['kube.node.memory.working_set_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.rss_bytes')
+      assert_equal @@parsed_string['node']['memory']['rssBytes'], @@hash_map_test['kube.node.memory.rss_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.page_faults')
+      assert_equal @@parsed_string['node']['memory']['pageFaults'], @@hash_map_test['kube.node.memory.page_faults'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.memory.major_page_faults')
+      assert_equal @@parsed_string['node']['memory']['majorPageFaults'], @@hash_map_test['kube.node.memory.major_page_faults'][2]["value"]
+
+    end
+
+    test 'test_emit_network_metrics' do
+
+      assert_not_nil @@hash_map_test.find('kube.node.network.rx_bytes')
+      assert_equal @@parsed_string['node']['network']['rxBytes'], @@hash_map_test['kube.node.network.rx_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.network.rx_errors')
+      assert_equal @@parsed_string['node']['network']['rxErrors'], @@hash_map_test['kube.node.network.rx_errors'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.network.tx_bytes')
+      assert_equal @@parsed_string['node']['network']['txBytes'], @@hash_map_test['kube.node.network.tx_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.network.tx_errors')
+      assert_equal @@parsed_string['node']['network']['txErrors'], @@hash_map_test['kube.node.network.tx_errors'][2]["value"]
+
+    end
+
+    test 'test_emit_fs_metrics' do
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.available_bytes')
+      assert_equal @@parsed_string['node']['fs']['availableBytes'], @@hash_map_test['kube.node.fs.available_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.capacity_bytes')
+      assert_equal @@parsed_string['node']['fs']['capacityBytes'], @@hash_map_test['kube.node.fs.capacity_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.used_bytes')
+      assert_equal @@parsed_string['node']['fs']['usedBytes'], @@hash_map_test['kube.node.fs.used_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes_free')
+      assert_equal @@parsed_string['node']['fs']['inodesFree'], @@hash_map_test['kube.node.fs.inodes_free'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes')
+      assert_equal @@parsed_string['node']['fs']['inodes'], @@hash_map_test['kube.node.fs.inodes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes_used')
+      assert_equal @@parsed_string['node']['fs']['inodesUsed'], @@hash_map_test['kube.node.fs.inodes_used'][2]["value"]
+
+    end
+
+    test 'test_emit_fs_imagefs_metrics' do
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.available_bytes')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['availableBytes'], @@hash_map_test['kube.node.imagefs.available_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.capacity_bytes')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['capacityBytes'], @@hash_map_test['kube.node.imagefs.capacity_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.used_bytes')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['usedBytes'], @@hash_map_test['kube.node.imagefs.used_bytes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes_free')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['inodesFree'], @@hash_map_test['kube.node.imagefs.inodes_free'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['inodes'], @@hash_map_test['kube.node.imagefs.inodes'][2]["value"]
+
+      assert_not_nil @@hash_map_test.find('kube.node.fs.inodes_used')
+      assert_equal @@parsed_string['node']['runtime']['imageFs']['inodesUsed'], @@hash_map_test['kube.node.imagefs.inodes_used'][2]["value"]
+
+    end
+
+  end
+
 end
