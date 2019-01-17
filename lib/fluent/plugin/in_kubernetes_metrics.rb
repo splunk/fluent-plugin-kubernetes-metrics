55f26297e1137cac3e03cd4924a79f8fd126b57b
Glen Patzlaff <glen@splunk.com>
2019-01-17 14:04:54 -0800

    Changed mispelled ca.cert to ca.crt

--- lib/fluent/plugin/in_kubernetes_metrics.rb
+++ lib/fluent/plugin/in_kubernetes_metrics.rb
@@ -138,7 +138,7 @@ module Fluent
 
         # Use SSL certificate and bearer token from Kubernetes service account.
         if Dir.exist?(@secret_dir)
-          secret_ca_file = File.join(@secret_dir, 'ca.cert')
+          secret_ca_file = File.join(@secret_dir, 'ca.crt')
           secret_token_file = File.join(@secret_dir, 'token')
 
           if @ca_file.nil? && File.exist?(secret_ca_file)
