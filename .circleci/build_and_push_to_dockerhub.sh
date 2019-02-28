117324692fd5b66980996eab29b585be087c59ca
Chaitanya Phalak <cphalak@splunk.com>
2019-02-28 13:12:45 -0800

    Added automated release changes

---
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+echo "Building docker image..."
+cp pkg/fluent-plugin-kubernetes-metrics-*.gem docker
+VERSION=`cat VERSION`
+docker build --build-arg VERSION=1.0.1 --no-cache -t splunk/fluent-plugin-kubernetes-metrics:metrics ./docker
+docker tag splunk/fluent-plugin-kubernetes-metrics:metrics $DOCKERHUB_ACCOUNT_ID/${DOCKERHUB_REPO_NAME}:${VERSION}
+echo "Push docker image to splunk dockerhub..."
+docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
+docker push $DOCKERHUB_ACCOUNT_ID/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
+echo "Docker image pushed successfully."
