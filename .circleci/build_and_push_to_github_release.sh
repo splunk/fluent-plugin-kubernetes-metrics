657c4b7e16db9ba78054cbffe02094b80831c40a
Chaitanya Phalak <cphalak@splunk.com>
2019-03-01 14:35:14 -0800

    fix

--- .circleci/build_and_push_to_github_release.sh
+++ .circleci/build_and_push_to_github_release.sh
@@ -6,5 +6,6 @@ sudo chmod +x ghr_v0.12.0_linux_amd64
 sudo mv ghr_v0.12.0_linux_amd64/ghr /usr/local/bin/ghr
 
 VERSION=`cat VERSION`
-echo "Pushing SCK release to github releases..."
-ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -n ${RELEASE_TITLE} -b ${RELEASE_BODY} -draft ${VERSION} ./pkg/
+echo "Pushing SCK release to github releases...${VERSION}"
+
+ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -n "${RELEASE_TITLE}" -b "${RELEASE_BODY}" -draft ${VERSION} ./pkg/
