e8d6e44f9fa4a3681fc8c72ca8d17826aafd7989
Rock Baek <rbaek@splunk.com>
2019-10-09 13:15:28 -0700

    Trigger release when VERSION file is edited

---
+++ .circleci/check_version_trigger_release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+#!/usr/bin/env bash
+LATEST_COMMIT=$(git rev-parse HEAD)
+VERSION_COMMIT=$(git log -1 --format=format:%H VERSION)
+if [ $VERSION_COMMIT = $LATEST_COMMIT ];
+    then
+        VERSION=`cat VERSION`
+        echo "VERSION is changed to $VERSION"
+        git checkout develop
+        git pull origin develop
+        git checkout -b release/$VERSION origin/develop
+        git push origin release/$VERSION
+        git checkout master
+        git merge release/$VERSION
+        git tag -a $VERSION -m 'Release Tag $VERSION'
+        git push origin master
+        git push origin --tags
+fi
