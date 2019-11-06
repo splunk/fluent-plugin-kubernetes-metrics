681f37871d191ddff31ad6880a6a966071402258
Rock Baek <rbaek@splunk.com>
2019-11-06 11:58:00 -0800

    fix

--- .circleci/trigger_func_test.sh
+++ .circleci/trigger_func_test.sh
@@ -8,20 +8,16 @@ BRANCH=master
 
 # Trigger functional test
 curl -X POST --header "Content-Type: application/json" \
-    -d '{"build_parameters": {"CIRCLE_JOB":"build_test", \
-                            "TRIG_BRANCH":"$CIRCLE_BRANCH", \
-                            "TRIG_PROJECT":"$CIRCLE_PROJECT_REPONAME", \
-                            "TRIG_REPO":"$CIRCLE_REPOSITORY_URL", \
-                        } \
-    }' "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT/tree/$BRANCH?circle-token=$CIRCLE_TOKEN" >>build.json
-
+    -d '{"build_parameters": {"CIRCLE_JOB":"build_test", "TRIG_BRANCH":"$CIRCLE_BRANCH", "TRIG_PROJECT":"$CIRCLE_PROJECT_REPONAME", "TRIG_REPO":"$CIRCLE_REPOSITORY_URL"}}' "https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT/tree/$BRANCH?circle-token=$CIRCLE_TOKEN" > build.json
+cat build.json
 BUILD_NUM=$(jq -r .build_num build.json)
 
 # Wait until finish or maximum 20 minutes
 TIMEOUT=20
 DONE="FALSE"
-until [TIMEOUT -lt 0 || "$DONE" == "TRUE" ]; do
-    curl https://circleci.com/api/v1.1/project/github/$ORGANIZATION/$PROJECT/$BUILD_NUM?circle-token=$CIRCLE_TOKEN >build_progress.json
+until [ "$TIMEOUT" -lt 0 ] || [ "$DONE" == "TRUE" ]; do
+    curl https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT/$BUILD_NUM?circle-token=$CIRCLE_TOKEN > build_progress.json
+    cat build_progress.json
     STATUS=$(jq -r .status build_progress.json)
     if [ "$STATUS" != "running" ]; then
         DONE="TRUE"
@@ -32,11 +28,11 @@ until [TIMEOUT -lt 0 || "$DONE" == "TRUE" ]; do
 done
 
 BUILD_URL=$(jq -r .build_url build_progress.json)
-if [ "$DONE" == "FALSE"]; then
+if [ "$DONE" == "FALSE" ]; then
     # Cancel hanging job and fail
-    curl -X POST https://circleci.com/api/v1.1/project/github/$ORGANIZATION/$PROJECT/$BUILD_NUM/cancel?circle-token=$CIRCLE_TOKEN
+    curl -X POST https://circleci.com/api/v1/project/$ORGANIZATION/$PROJECT/$BUILD_NUM/cancel?circle-token=$CIRCLE_TOKEN
 else
-    if [ "$STATUS" == "success"]; then
+    if [ ! "$STATUS" == "success" ]; then
         echo "Functional test have failed please see:"
         echo $BUILD_URL
         exit 1
